4e7f9869be4ff9889b721abf41dacf2b703273cf
akmalikhsan <63221748+AkmalFounder@users.noreply.github.com>
2022-01-14 07:50:56 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import datetime
 
-server_ip = "0.0.0.0"
+server_ip = "127.0.0.1"
 server_port = 8008
 
 with SimpleXMLRPCServer((server_ip, server_port), allow_none=True) as server:
@@ -11,35 +11,36 @@ with SimpleXMLRPCServer((server_ip, server_port), allow_none=True) as server:
         def __init__(self):
             self.nasabah = [
                 {
-                    "nama": "ABC DEF",
-                    "nomor_kartu": "1234123412341234",
-                    "nomor_rekening": "123123123",
+                    "nama": "Akmal Ikhsan",
+                    "nomor_kartu": "12344321",
+                    "nomor_rekening": "123321456",
                     "pin": "123456",
                     "saldo": 1000000,
                     "history": [
-                        {"tanggal": "01/06/2021", "uraian": "transfer dari ZER LPO", "nominal": 20000},
-                        {"tanggal": "04/06/2021", "uraian": "tarik tunai", "nominal": -150000},
-                        {"tanggal": "06/06/2021", "uraian": "top up echannel kartu", "nominal": -50000},
-                        {"tanggal": "10/06/2021", "uraian": "transfer ke FED CBA", "nominal": -120000},
-                        {"tanggal": "10/06/2021", "uraian": "top up echannel", "nominal": -130000},
-                        {"tanggal": "17/06/2021", "uraian": "top up gopay", "nominal": -100000},
-                        {"tanggal": "20/06/2021", "uraian": "transfer dari LL PAK", "nominal": 56000},
-                        {"tanggal": "21/06/2021", "uraian": "top up echannel", "nominal": -250000},
+                        {"tanggal": "01/01/2022", "uraian": "transfer dari Batik Air", "nominal": 20000},
+                        {"tanggal": "02/01/2022", "uraian": "transfer dari AirIndo", "nominal": 20000},
+                        {"tanggal": "03/01/2022", "uraian": "tarik tunai", "nominal": -150000},
+                        {"tanggal": "03/01/2022", "uraian": "top up echannel kartu", "nominal": -50000},
+                        {"tanggal": "04/01/2022", "uraian": "transfer ke Jokowi", "nominal": -120000},
+                        {"tanggal": "05/01/2022", "uraian": "top up echannel", "nominal": -130000},
+                        {"tanggal": "05/01/2022", "uraian": "top up gopay", "nominal": -100000},
+                        {"tanggal": "06/01/2022", "uraian": "transfer dari Jerompolin", "nominal": 56000},
+                        {"tanggal": "06/01/2022", "uraian": "top up echannel", "nominal": -250000},
                     ]
                 },
                 {
-                    "nama": "FED CBA",
+                    "nama": "Salsabila Anza",
                     "nomor_kartu": "4321432143214321",
                     "nomor_rekening": "321321321",
                     "pin": "654321",
                     "saldo": 2000000,
                     "history": [
-                        {"tanggal": "01/06/2021", "uraian": "biaya administrasi", "nominal": -5000},
-                        {"tanggal": "09/06/2021", "uraian": "top up echannel", "nominal": -120000},
-                        {"tanggal": "10/06/2021", "uraian": "transfer dari ABC DEF", "nominal": 130000},
-                        {"tanggal": "11/06/2021", "uraian": "top up echannel", "nominal": -100000},
-                        {"tanggal": "17/06/2021", "uraian": "transfer ke POPLO", "nominal": -20000},
-                        {"tanggal": "20/06/2021", "uraian": "top up shopeepay", "nominal": -350000},
+                        {"tanggal": "01/01/2022", "uraian": "biaya administrasi", "nominal": -5000},
+                        {"tanggal": "09/01/2022", "uraian": "top up echannel", "nominal": -120000},
+                        {"tanggal": "10/01/2022", "uraian": "transfer dari ABC DEF", "nominal": 130000},
+                        {"tanggal": "11/01/2022", "uraian": "top up echannel", "nominal": -100000},
+                        {"tanggal": "13/01/2022", "uraian": "transfer ke POPLO", "nominal": -20000},
+                        {"tanggal": "02/01/2022", "uraian": "top up shopeepay", "nominal": -350000},
                     ]
                 },
             ]
